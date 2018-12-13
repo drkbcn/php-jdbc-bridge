e60052b5038b74e1103646c59dd7714b9bd8c28d
Jon Gaydos <15269036+jgaydos@users.noreply.github.com>
2018-12-13 11:16:26 -0500

    Update PJBridge.php

--- php/PJBridge.php
+++ php/PJBridge.php
@@ -23,27 +23,27 @@
 class PJBridge {
 
 	private $sock;
-        private $jdbc_enc;
-        private $app_enc;
+	private $jdbc_enc;
+	private $app_enc;
 
-        public $last_search_length = 0;
+	public $last_search_length = 0;
 
-	function __construct($host="localhost", $port="4444", $jdbc_enc="ascii", $app_enc="ascii") {
-
+	public function __construct(string $host = 'localhost', int $port = 4444, string $jdbc_enc = 'ascii', string $app_enc = 'ascii')
+	{
 		$this->sock = fsockopen($host, $port);
-                $this->jdbc_enc = $jdbc_enc;
-                $this->app_enc = $app_enc;
- 	}
+		$this->jdbc_enc = $jdbc_enc;
+		$this->app_enc = $app_enc;
+	}
 
-	function __destruct() {
-	
+	public function __destruct()
+	{
 		fclose($this->sock);
 	}
 
-	private function parse_reply() {
-
+	private function parse_reply()
+	{
 		$il = explode(' ', fgets($this->sock));
-		$ol = array();
+		$ol = [];
 
 		foreach ($il as $value)
 			$ol[] = iconv($this->jdbc_enc, $this->app_enc, base64_decode($value));
@@ -51,92 +51,76 @@ class PJBridge {
 		return $ol;
 	}
 
-	private function exchange($cmd_a) {
-
+	private function exchange(array $cmd_a)
+	{
 		$cmd_s = '';
 
 		foreach ($cmd_a as $tok)
 			$cmd_s .= base64_encode(iconv($this->app_enc, $this->jdbc_enc, $tok)).' ';
-		
-		$cmd_s = substr($cmd_s, 0, -1)."\n";
 
+		$cmd_s = substr($cmd_s, 0, -1)."\n";
 		fwrite($this->sock, $cmd_s);
-		
 		return $this->parse_reply();
 	}
 
-	public function connect($url, $user, $pass) {
-
-		$reply = $this->exchange(array('connect', $url, $user, $pass));
+	public function connect(string $url, string $user, string $pass)
+	{
+		$reply = $this->exchange(['connect', $url, $user, $pass]);
 
 		switch ($reply[0]) {
-
-		case 'ok':
-			return true;
-
-		default:
-			return false;
+			case 'ok':
+				return true;
+			default:
+				return false;
 		}
 	}
 
-	public function exec($query) {
-
-		$cmd_a = array('exec', $query);
+	public function exec(string $query)
+	{
+		$cmd_a = ['exec', $query];
 
 		if (func_num_args() > 1) {
-		
 			$args = func_get_args();
-
-			for ($i = 1; $i < func_num_args(); $i ++)
+			for ($i = 1; $i < func_num_args(); $i++)
 				$cmd_a[] = $args[$i];
 		}
 
 		$reply = $this->exchange($cmd_a);
 
 		switch ($reply[0]) {
-
-		case 'ok':
-			return $reply[1];
-
-		default:
-			return false;
+			case 'ok':
+				return $reply[1];
+			default:
+				return false;
 		}
 	}
 
-	public function fetch_array($res) {
-
-		$reply = $this->exchange(array('fetch_array', $res));
+	public function fetch_array(string $res)
+	{
+		$reply = $this->exchange(['fetch_array', $res]);
 
 		switch ($reply[0]) {
-
-		case 'ok':
-			$row = array();
-
-			for ($i = 0; $i < $reply[1]; $i ++) {
-
-				$col = $this->parse_reply($this->sock);
-				$row[$col[0]] = $col[1];
-			}
-
-			return $row;
-
-		default:
-			return false;
+			case 'ok':
+				$row = [];
+				for ($i = 0; $i < $reply[1]; $i++) {
+					$col = $this->parse_reply($this->sock);
+					$row[$col[0]] = $col[1];
+				}
+				return $row;
+			default:
+				return false;
 		}
 	}
 
-	public function free_result($res) {
-
-		$reply = $this->exchange(array('free_result', $res));
+	public function free_result(string $res)
+	{
+		$reply = $this->exchange(['free_result', $res]);
 
 		switch ($reply[0]) {
-
-		case 'ok':
-			return true;
-		default:
-			return false;
+			case 'ok':
+				return true;
+			default:
+				return false;
 		}
 	}
 }
-
-?>
